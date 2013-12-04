2d671df83e822a172ce07933ce2b87ac54a437c6
Cong  Wang <congwang.5h@gmail.com>
2013-12-04 08:58:34 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -1,1 +1,27 @@
-fff
+#ifndef JEU_H_INCLUDED
+#define JEU_H_INCLUDED
+
+const int MAX=8;
+const char B='B';
+const char N='N';
+const char V='X';
+
+class plateau
+{
+public:
+    void afficher();
+private:
+    char p[MAX][MAX]=
+    {
+        {V,V,V,V,V,V,V,V},
+        {V,V,V,V,V,V,V,V},
+        {V,V,V,V,V,V,V,V},
+        {V,V,V,B,N,V,V,V},
+        {V,V,V,N,B,V,V,V},
+        {V,V,V,V,V,V,V,V},
+        {V,V,V,V,V,V,V,V},
+        {V,V,V,V,V,V,V,V},
+    };
+};
+
+#endif // JEU_H_INCLUDED
