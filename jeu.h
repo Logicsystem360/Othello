a3f875a809b086e8181d15b2c0358a96fcf7e052
Logicsystem360 <Logicsystem360@gmail.com>
2013-12-04 10:00:56 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -1,27 +1,6 @@
+//Fichier pour les déclarations de la classe jeu
+
 #ifndef JEU_H_INCLUDED
 #define JEU_H_INCLUDED
-
-const int MAX=8;
-const char B='B';
-const char N='N';
-const char V='X';
-
-class plateau
-{
-public:
-    void afficher();
-private:
-    char p[MAX][MAX]=
-    {
-        {V,V,V,V,V,V,V,V},
-        {V,V,V,V,V,V,V,V},
-        {V,V,V,V,V,V,V,V},
-        {V,V,V,B,N,V,V,V},
-        {V,V,V,N,B,V,V,V},
-        {V,V,V,V,V,V,V,V},
-        {V,V,V,V,V,V,V,V},
-        {V,V,V,V,V,V,V,V},
-    };
-};
 
 #endif // JEU_H_INCLUDED
