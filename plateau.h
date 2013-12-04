195120570b94973f90ac60717752459751f614a9
Logicsystem360 <Logicsystem360@gmail.com>
2013-12-04 09:59:27 +0100

    Update plateau.h

--- plateau.h
+++ plateau.h
@@ -1,1 +1,29 @@
 //Fichier de déclaration pour la classe Plateau
+
+#ifndef PLATEAU_H_INCLUDED
+#define PLATEAU_H_INCLUDED
+
+const int MAX=8;
+const char B='B';
+const char N='N';
+const char V='X';
+
+class plateau
+{
+public:
+    void afficher();
+private:
+    char p[MAX][MAX]=
+    {
+        {V,V,V,V,V,V,V,V},
+        {V,V,V,V,V,V,V,V},
+        {V,V,V,V,V,V,V,V},
+        {V,V,V,B,N,V,V,V},
+        {V,V,V,N,B,V,V,V},
+        {V,V,V,V,V,V,V,V},
+        {V,V,V,V,V,V,V,V},
+        {V,V,V,V,V,V,V,V},
+    };
+};
+
+#endif // PLATEAU_H_INCLUDED
