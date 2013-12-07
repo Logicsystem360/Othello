7408843850378625a2dfef61255e8b5af54bbb40
Cong  Wang <congwang.5h@gmail.com>
2013-12-07 17:25:19 +0100

    Update joueur.h
    modifier .h

--- joueur.h
+++ joueur.h
@@ -3,15 +3,24 @@
 #ifndef JOUEUR_H_INCLUDED
 #define JOUEUR_H_INCLUDED
 
+#include <iostream>
+#include "main.h"
+#include "jeu.h"
+#include "Plateau.h"
+#include "joueur.h"
 
-class Joueur
+using namespace std;
+
+class joueur
 {
 public:
-    std::string nom;
-    int score;
-    void saisir();
-    void couleur();
-    char coul;
+    joueur(string nomstd="NULL",char coulstd='N',int scorestd=0);
+    void saisir(string);
+    void afficher(string);
+private:
+    string nom ; // le nom du joueur
+    char coul ; // caractère représentant le joueur : ‘N’ ou ‘B’
+    int score ; // le score du joueur pour la partie en cours
 };
 
 #endif // JOUEUR_H_INCLUDED
