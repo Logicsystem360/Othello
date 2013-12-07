097a5ebad94bd7bb6f1c0ece7e06fd22da0fdc78
Cong  Wang <congwang.5h@gmail.com>
2013-12-07 17:24:19 +0100

    Update joueur.cpp
    ajouté saisir et afficher les informations de joueur

--- joueur.cpp
+++ joueur.cpp
@@ -1,19 +1,71 @@
 // Fichier pour les fonctions de la classe joueur
 
 #include <iostream>
-#include <string>
+#include "main.h"
 #include "jeu.h"
+#include "Plateau.h"
+#include "joueur.h"
 
 using namespace std;
 
-void Joueur::saisir()
+
+
+///////////////////////////////////
+//constructeur de classe "joueur"//
+///////////////////////////////////
+
+joueur::joueur(string nomstd,char coulstd,int scorestd)
 {
-    cout << "Veuillez saisir votre nom de joueur !" << endl;
-    cin >> nom;
+    nom=nomstd;
+    coul=coulstd;
+    score=scorestd;
 }
 
-void Joueur::couleur()
+/////////////////////////////////////////////
+//Methode saisir des information de joueur.//
+/////////////////////////////////////////////
+
+
+void joueur::saisir(string nomJoueur)
 {
-    cout << "Choix de la couleur ! (N pour Noir ou B pour Blanc)" << endl;
+    //saisir nom de joueur.
+    cout << "Entrez le nom de " << nomJoueur << endl;
+    cout << "Nom : ";
+    cin >> nom;
+
+    //saisir couleur choisi.
+    cout << "Entrez couleur que vous voulez choisir.('N' pour Noir ou 'B' pour Blanc) " << endl;
+    cout << "Couleur choisi : ";
     cin >> coul;
+
+    //si joueur ne saisi ni 'B' ni 'N'.
+    while (coul!='B'&&coul!='N')
+    {
+        cout << "Entrez couleur que vous voulez choisir,svp.('N' pour Noir ou 'B' pour Blanc)" << endl;
+        cout << "Couleur choisi : ";
+        cin >> coul;
+    }
+
+    //Au debut,le score est toujour 0.Il ne faut pas de saisir.
+
+
+    cout << endl;
 }
+
+////////////////////////////////////////////////
+//Methode afficher les informations de joueur.//
+////////////////////////////////////////////////
+
+
+void joueur::afficher(string nomjoueur)
+{
+    //afficher le nom de joueur.
+    cout << "Nom de joueur " << nomjoueur << " : " << nom <<endl;
+
+    //afficher le couleur choisi par joueur.
+    cout << "Couleur choisi :" << coul << endl;
+
+
+    cout << endl;
+}
+
