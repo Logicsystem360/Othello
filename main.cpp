34aec520d0cb4ee96824bd608df85c0035c0192b
Cong  Wang <congwang.5h@gmail.com>
2013-12-07 19:02:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include "main.h"
 #include "jeu.h"
-#include "plateau.h"
+#include "Plateau.h"
 #include "joueur.h"
 
 using namespace std;
 
 int main()
 {
+    joueur j1,j2;
+    j1.saisir("joueur1");
+    j1.afficher("joueur1");
+    j2.saisir("joueur2");
+    j2.afficher("joueur2");
     plateau p;
     p.afficher();
     return 0;
